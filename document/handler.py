5893314c82732a76a81aa04a20d039c25b2497eb
andela-sjames <samuel.james@andela.com>
2019-06-06 12:51:43 +0100

    Define first version lambda function.

--- document/handler.py
+++ document/handler.py
@@ -1,24 +1,31 @@
 import json
+from urllib import request, error, parse
 
 
-def hello(event, context):
+def get_page_title_handler(event, context):
+
+    failure = None
+    page_title = "NO TITLE"
+    url = event['page_url']
+    try:
+        webpage = request.urlopen(url).read()
+        try:
+            page_title = str(webpage).split('<title>')[1].split('</title>')[0]
+        except Exception as e:
+            failure = str(e)
+    except Exception as e:
+        failure = str(e)
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "page_title": page_title,
+        "failure": failure
     }
 
+    statusCode = 400 if failure else 200
+        
     response = {
-        "statusCode": 200,
+        "statusCode": statusCode,
         "body": json.dumps(body)
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
